1ad84cd1ffa67f5100faa0a3911e4fb4734528cf
USER <USER@DESKTOP-9G6FTRG>
2020-06-01 16:39:04 +0300

    16:36 6.1

--- AssigmentCode/src/controller/MainMenuController.java
+++ AssigmentCode/src/controller/MainMenuController.java
@@ -1,71 +1,131 @@
 package controller;
 
-import java.awt.Insets;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-
-import javafx.application.Application;
+import com.jfoenix.controls.JFXTabPane;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Side;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.TextAlignment;
-import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainMenuController {
+
+    @FXML
+    private JFXTabPane tabContainer;
+
+    @FXML
+    private Tab userProfileTab;
+
+    @FXML
+    private AnchorPane userProfileContainer;
+
+    @FXML
+    private Tab settingsTab;
+
+    @FXML
+    private AnchorPane settingsContainer;
+
+    @FXML
+    private Tab logoutTab;
 
 
-public class MainMenuController extends Application implements Initializable {
-public static void main(String[] args) {
-	launch(args);
-}
-//	@FXML
-//	private JFXTabPane tabContainer;
+    private double tabWidth = 90.0;
+    public static int lastSelectedTabIndex = 0;
 
-	@FXML
-	private Tab userProfileTab;
+    /// Life cycle
 
-	@FXML
-	private Tab settingsTab;
+    @FXML
+    public void initialize() {
+        configureView();
+    }
 
-	@FXML
-	private Tab logoutTab;
+    /// Private
 
-	/// 2.
-	private double tabWidth = 90.0;
-	public static int lastSelectedTabIndex = 0;
+    private void configureView() {
+        tabContainer.setTabMinWidth(tabWidth);
+        tabContainer.setTabMaxWidth(tabWidth);
+        tabContainer.setTabMinHeight(tabWidth);
+        tabContainer.setTabMaxHeight(tabWidth);
+        tabContainer.setRotateGraphic(true);
 
-	
+        EventHandler<Event> replaceBackgroundColorHandler = event -> {
+            lastSelectedTabIndex = tabContainer.getSelectionModel().getSelectedIndex();
 
-	
+            Tab currentTab = (Tab) event.getTarget();
+            if (currentTab.isSelected()) {
+                currentTab.setStyle("-fx-background-color: -fx-focus-color;");
+            } else {
+                currentTab.setStyle("-fx-background-color: -fx-accent;");
+            }
+        };
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// Parent root =
-	
-		// FXMLLoader.load(getClass().getResource("/application/ServerPort.fxml"));
-		Parent root = FXMLLoader.load(getClass().getResource("/controller/MMA.fxml"));
-		Scene scene = new Scene(root);
-		// scene.getStylesheets().add(getClass().getResource("/gui/ServerPort.css").toExternalForm());
-		primaryStage.setTitle("Client");
-		primaryStage.setScene(scene);
-		
-		primaryStage.show();
-	}
+        EventHandler<Event> logoutHandler = event -> {
+            Tab currentTab = (Tab) event.getTarget();
+            if (currentTab.isSelected()) {
+                tabContainer.getSelectionModel().select(lastSelectedTabIndex);
 
+                // TODO: logout action
+                // good place to show Dialog window with Yes / No question
+                System.out.println("Logging out!");
+            }
+        };
 
+        configureTab(userProfileTab, "User\nProfile", "/application/user-profile.png", userProfileContainer, getClass().getResource("userprofile.fxml"), replaceBackgroundColorHandler);
+        configureTab(settingsTab, "Settings", "/application/settings.png", settingsContainer, getClass().getResource("settings.fxml"), replaceBackgroundColorHandler);
+        configureTab(logoutTab, "Logout", "/application/logout.png", null, null, logoutHandler);
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		//tabContainer.setSide(Side.LEFT);
-	}
+        userProfileTab.setStyle("-fx-background-color: -fx-focus-color;");
+    }
+
+    private void configureTab(Tab tab, String title, String iconPath, AnchorPane containerPane, URL resourceURL, EventHandler<Event> onSelectionChangedEvent) {
+        double imageWidth = 40.0;
+
+        ImageView imageView = new ImageView(new Image(iconPath));
+        imageView.setFitHeight(imageWidth);
+        imageView.setFitWidth(imageWidth);
+
+        Label label = new Label(title);
+        label.setMaxWidth(tabWidth - 20);
+        label.setPadding(new Insets(5, 0, 0, 0));
+        label.setStyle("-fx-text-fill: black; -fx-font-size: 8pt; -fx-font-weight: normal;");
+        label.setTextAlignment(TextAlignment.CENTER);
+
+        BorderPane tabPane = new BorderPane();
+        tabPane.setRotate(90.0);
+        tabPane.setMaxWidth(tabWidth);
+        tabPane.setCenter(imageView);
+        tabPane.setBottom(label);
+
+        tab.setText("");
+        tab.setGraphic(tabPane);
+
+        tab.setOnSelectionChanged(onSelectionChangedEvent);
+
+        if (containerPane != null && resourceURL != null) {
+            try {
+                Parent contentView = FXMLLoader.load(resourceURL);
+                containerPane.getChildren().add(contentView);
+                AnchorPane.setTopAnchor(contentView, 0.0);
+                AnchorPane.setBottomAnchor(contentView, 0.0);
+                AnchorPane.setRightAnchor(contentView, 0.0);
+                AnchorPane.setLeftAnchor(contentView, 0.0);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
